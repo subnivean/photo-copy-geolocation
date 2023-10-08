49bf35acaef313a0067f2671181109e48e80c4f9
Mark Krautheim <mark.krautheim@gmail.com>
2023-10-08 12:56:37 -0400

    Add Qt test program

---
+++ bin/test.py
@@ -0,0 +1,21 @@
+# Import PySide6 classes
+import sys
+from PySide6 import QtCore, QtWidgets
+
+# Create a Qt application
+app = QtWidgets.QApplication(sys.argv)
+
+# Create a Window
+mywindow = QtWidgets.QWidget()
+mywindow.resize(320, 240)
+mywindow.setWindowTitle("Hello, World!")
+
+# Create a label and display it all together
+mylabel = QtWidgets.QLabel(mywindow)
+mylabel.setText("Hello, World!")
+mylabel.setGeometry(QtCore.QRect(200, 200, 200, 200))
+
+mywindow.show()
+
+# Enter Qt application main loop
+sys.exit(app.exec())
