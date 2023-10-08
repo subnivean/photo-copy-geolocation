d36717f49db22d9e71ea4c136f87a8c15f2e0008
Mark Krautheim <mark.krautheim@gmail.com>
2023-10-08 17:18:37 -0400

    Add file for batch processing of directory

---
+++ bin/batch.py
@@ -0,0 +1,72 @@
+# Process an entire directory of images (presumably from the same day), and copy the
+# GPS data from all files lacking it from the file nearest in time.
+
+from datetime import datetime
+from pathlib import Path
+import sys
+import pyexiv2
+
+EXTS = [".jpg", ".arw"]
+DTFMT = "%Y:%m:%d %H:%M:%S"
+BASEDIR = "/home/mark/Pictures/2023/20230917.test"
+
+# Pictures with a time delta greater than the following
+# will not be modified
+MAXDELTASECS = 600
+
+imgfiles = sorted([p for p in Path(BASEDIR).glob("*") if p.suffix in EXTS])
+
+goodfiles, badfiles = [], []
+for imgfile in imgfiles:
+    # img = Image(imgfile)
+    img = pyexiv2.Image(imgfile.as_posix())
+    alldata = img.read_exif()
+    tstamp = datetime.strptime(alldata["Exif.Image.DateTime"], DTFMT)
+
+    data = {k: v for k, v in alldata.items() if "GPS" in k}
+    data.update(filepath=imgfile)
+    data.update(tstamp=tstamp)
+
+    if "Exif.GPSInfo.GPSLongitude" in data:
+        print(f"Found GPS data in {imgfile}")
+        goodfiles.append(data)
+    else:
+        print(f"No GPS data in {imgfile}")
+        badfiles.append(data)
+
+if len(badfiles) == 0:
+    print("Nothing to modify, exiting")
+    sys.exit()
+
+if len(goodfiles) == 0:
+    print("No donor files found, exiting")
+    sys.exit()
+
+for bf in badfiles:
+    print(f"{bf['filepath']}")
+    bts = bf["tstamp"]
+    mindsecs = 1e6
+    minndx = -1
+    for ndx, gf in enumerate(goodfiles):
+        dsecs = abs((gf["tstamp"] - bts).total_seconds())
+        if dsecs < mindsecs:
+            mindsecs = dsecs
+            minndx = ndx
+        # print(f"{gf['filepath']=}, {dsecs=}")
+
+    print(f"{minndx=}")
+
+    if mindsecs < MAXDELTASECS:
+        donordata = goodfiles[minndx]
+        gpsdata = {k: v for k, v in donordata.items() if "GPS" in k}
+        # recvimg = Image(bf["filepath"])
+        # for k, v in gpsdata.items():
+        #     setattr(recvimg, k, v)
+        p = Path(bf["filepath"])
+        newname = f"{p.stem}_new{p.suffix}"
+        newfile = p.with_name(newname)
+        # with open(newfile, "wb") as fh:
+        #     fh.write(recvimg.get_file())
+
+    else:
+        print("Too far apart - not changing")
